698e1dad3647048396e95ed34c294088917de73c
apolline <apolline.fontaine@epitech.eu>
2023-01-09 16:49:27 +0100

    connection serv and comm between 2 shells + pygame interface

---
+++ client.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+import pygame
+from pygame.locals import *
+from socket import socket
+import os
+
+is_closed = False
+
+server = socket()
+
+server.connect(("127.0.0.1", 5000))
+
+print("You are connected")
+pygame.init()
+window = pygame.display.set_mode((640, 480), RESIZABLE)
+
+bg = pygame.image.load("assets/background.jpg").convert()
+window.blit(bg, (0,0))
+
+pygame.display.flip()
+
+game = 1
+
+while game:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game = 0
