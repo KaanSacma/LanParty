22b9a6da889929549cc8a3fdbca16fc171078537
apolline <apolline.fontaine@epitech.eu>
2023-01-11 22:18:00 +0100

    i need a new branch

--- src/client.py
+++ src/client.py
@@ -1,12 +1,34 @@
 #!/usr/bin/python3
 
-# import pygame
-# from pygame.locals import *
 from socket import socket
 import os
 from main import main
 
+res = False
 is_closed = False
+mutex = Lock()
+
+def send_msg(server):
+    global is_closed, mutex
+    msg = ""
+    while (msg):
+        msg = input("")
+        if(len(msg) > 0):
+            server.send(msg.encode())
+    mutex.acquire()
+    is_closed = True
+    mutex.release()    
+
+def receiv_msg(server):
+    global is_closed, mutex
+    msg = ""
+    while (msg):
+        msg = server.recv(1024).decode()
+        if (len(msg) > 0):
+            print('\n' + msg)
+    mutex.acquire()
+    is_closed = True
+    mutex.release()
 
 server = socket()
 
@@ -15,3 +37,18 @@ server.connect(("127.0.0.1", 5505))
 print("You are connected")
 
 main()
+
+res = False
+while(True):
+    mutex.acquire()
+    res = is_closed
+    mutex.release()
+    if (res == True):
+        server.close()
+        os._exit(0)
+
+thread_one = Thread(target=send_msg, args=(server,))
+thread_two = Thread(target=receiv_msg, args=(server,))
+
+thread_one.start()
+thread_two.start()
